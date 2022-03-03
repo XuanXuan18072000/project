c7e008afd30b2758f4f871eb256917e2d62a47a4
XuanXuan18072000 <xuan.01299260679@gmail.com>
2022-03-03 16:03:44 +0700

    capnhat3/3

--- src/firebase.config.jsx
+++ src/firebase.config.jsx
@@ -2,9 +2,9 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 firebase.initializeApp({
-  apiKey: 'AIzaSyCvHWHCbQj6jmtaUrhE5mSByzSfx19bQYwz',
-  authDomain: 'project1-48e71.firebaseapp.com',
-  projectId: 'project1-48e71'
+  apiKey: "AIzaSyC2TPtb2L8g22S2G00AXKWIByFXBJy4AmE",
+  authDomain: "project-alta.firebaseapp.com",
+  projectId: "project-alta",
 });
 var db = firebase.firestore();
 export default db;
