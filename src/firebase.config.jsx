58876a0cc06dffea757e4a138bc4cc0ea849d494
XuanXuan18072000 <xuan.01299260679@gmail.com>
2022-04-07 16:11:03 +0700

    hoanthanh

--- src/firebase.config.jsx
+++ src/firebase.config.jsx
@@ -2,9 +2,9 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 firebase.initializeApp({
-  apiKey: 'AIzaSyALmyveoiOmR5ZG87Mx9Imk3XgkCWi0sO4',
-  authDomain: 'react-firebase-fdf84.firebaseapp.com',
-  projectId: 'react-firebase-fdf84'
+  apiKey: "AIzaSyC2TPtb2L8g22S2G00AXKWIByFXBJy4AmE",
+  authDomain: "project-alta.firebaseapp.com",
+  projectId: "project-alta",
 });
 var db = firebase.firestore();
 export default db;
